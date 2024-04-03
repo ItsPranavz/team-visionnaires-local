e783f7c425e9b3ed0bf39ae2d69200a631fd6677
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-04-03 22:01:05 +0530

    Added contact component

---
+++ website/src/components/ContactForm.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {useState} from 'react';
+import '../App.css';
+const ContactForm = () => {
+  const [formStatus, setFormStatus] = useState('Send')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setFormStatus('Submitting...')
+    const { name, email,contact,company, message } = e.target.elements
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      contact:contact.value,
+      company:company.value,
+      message: message.value,
+    }
+    console.log(conFom)
+  }
+  return (
+    <div className="container mt-5 form-container">
+     <h2>Contact Us</h2>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input className="form-control" type="text" id="name" required />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input className="form-control" type="email" id="email" required />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="contact">
+            Contact Number
+          </label>
+          <input className="form-control" type="contact" id="contact" required />
+        </div>
+         
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="company">
+            Company Name
+          </label>
+          <input className="form-control" type="company" id="company" required />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="message">
+            Tell us More
+          </label>
+          <textarea className="form-control" id="message" rows="5"  />
+        </div>
+        <div class="button"> 
+        <button className="btn btn-danger" type="submit">
+          {formStatus}
+        </button>
+
+        </div>
+        
+      </form>
+    </div>
+  )
+}
+export default ContactForm;
