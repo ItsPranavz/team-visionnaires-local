ebde5a2000c05aa8a99b651affbeb24d90f1e7e8
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-04-04 00:09:48 +0530

    Added footer component

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import ContactForm from './components/ContactForm.jsx';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer.jsx';
 function App() {
   return (
     <>
-        <ContactForm/>
+        <Footer/>
     </>
   );
 }
