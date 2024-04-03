0a91bf109bb15e87d49389ac3170ce8b950ee254
Pranav Sharma <pranavsharma.bt22cse@pec.edu.in>
2024-04-04 01:27:57 +0530

    Added header and adjusted contact form

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import ContactForm from './components/ContactForm.jsx';
+import Header from './components/Header.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <>
+        <Header />
         <ContactForm/>
     </>
   );
