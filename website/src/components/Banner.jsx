89c688c9b4cbe4948957f194c8b38dfa312854d9
Pranav Sharma <pranavsharma.bt22cse@pec.edu.in>
2024-04-04 16:58:14 +0530

    Fixed responsiveness errors of banner

--- website/src/components/Banner.jsx
+++ website/src/components/Banner.jsx
@@ -9,8 +9,8 @@ function Banner() {
         <div className="banner-container">
             <img className="banner-image" src={img} />
             <div className="container-column">
-                <h5><Typewriter text="FINDING THE RIGHT SOLUTION FOR YOUR BUSINESS IN SUPPLY CHAIN ANALYSIS, TRANSPORT MONIT0RING, DEMAND FORECASTING" delay={1000} /></h5>
-                <p>"We are aiming to build a Supply Chain Management Solution, which will help all the stakeholders of the Supply Chain to manage and access data of quantity ordered and how much to keep in buffer stock, in order to mitigate the impact of "BullWhip" effect."</p>
+                <p className="banner-heading"><Typewriter text="FINDING THE RIGHT SOLUTION FOR YOUR BUSINESS IN SUPPLY CHAIN ANALYSIS, TRANSPORT MONIT0RING, DEMAND FORECASTING" delay={1000} /></p>
+                <p className="banner-content">"We are aiming to build a Supply Chain Management Solution, which will help all the stakeholders of the Supply Chain to manage and access data of quantity ordered and how much to keep in buffer stock, in order to mitigate the impact of "BullWhip" effect."</p>
             </div>
         </div>
     )
