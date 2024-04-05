8955f3f4a0bcb280f04f945fde59b4c19aa98d5c
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-04-05 11:39:05 +0530

    Fixed import conflicts

--- website/src/App.js
+++ website/src/App.js
@@ -4,14 +4,10 @@ import Footer from './components/Footer.jsx';
 import Banner from './components/Banner.jsx';
 import Data from './components/Data.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import  WhyChoose  from '../src/components/WhyChoose.jsx';
-=======
 import  Features  from '../src/components/Features.jsx';
 import Heading from './components/Heading.jsx';
 import WhySection from './components/WhySection.jsx';
 
->>>>>>> 5dcd31a57a884fabae9711ced4f75945b53f1380
 function App() {
   return (
     <>
