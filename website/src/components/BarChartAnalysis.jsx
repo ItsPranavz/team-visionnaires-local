c7eb5b55f8c649daff503cd16bcfbab3a892c66d
Pranav Sharma <pranavsharma.bt22cse@pec.edu.in>
2024-04-06 05:32:04 +0530

    Generalized bar chart analysis component

--- website/src/components/BarChartAnalysis.jsx
+++ website/src/components/BarChartAnalysis.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 
-function BarChartAnalysis() {
+function BarChartAnalysis(props) {
   return (
     <BarChart
       series={[
-        { data: [35,45,55,65] },
+        { data: props.data.map(item => item.quantity) },
         
       ]}
       height={490}
-      xAxis={[{ data: ['Q1','Q2','Q3','Q4'], scaleType: 'band' }]}
+      xAxis={[{ data: props.data.map(item => item.month), scaleType: 'band' }]}
       margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
     />
   );
